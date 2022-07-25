e0e1c4cedbcc2cb32d8436546dfcde438e8f9423
RenWuoXin <100norlan@gmail.com>
2022-07-25 11:09:33 +0800

    update

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ app.get("/", function (req, res) {
     res.render("index");
 });
 
-// app.get('/pair', function (req, res) {
-//     // add data property to about page
-//     res.render('pair', { data: data.user });
-// });
+
 
 app.get("/pair", function (req, res) {
     let sql = "SELECT * FROM members where Gender = ? ORDER BY Rand() limit 4";
@@ -25,51 +22,7 @@ app.get("/pair", function (req, res) {
 
 
 
-// app.get("/pair", function (req, res) {
-//     var gender = req.body.gender;
-//     var minage = req.body.minage;
-//     var maxage = req.body.maxage;
-//     var county = req.body.county;
-//     let params = [gender, county];
-//     let sql = "SELECT * FROM member where Gender =? AND CityId =? ORDER BY Rand() limit 4";
-//     con.query(sql, function (err, result, fields) {
-//         if (err) console.log("query is not excuted. select fail...\n" + err);
-//         else res.render("pair", { data: result });
-//     });
-// });
 
-// app.post('/pair', function (req, res) {
-
-//     var gender = req.body.gender;
-//     var minage = req.body.minage;
-//     var maxage = req.body.maxage;
-//     var county = req.body.county;
-
-//     con.connect(function (error) {
-//         console.log(error);
-
-//         var sql = "SELECT * FROM member where Gender =" + gender + " AND WHERE CityId =" + county + " ORDER BY Rand() limit 4";
-
-//         var values = [
-//             [gender, minage, maxage, county]
-//         ];
-//         con.query(sql, [values], function (error, result) {
-//             console.log(error);
-//             res.res.render("pair", { data: result });
-//         })
-//     });
-// });
-// app.get("/match", function (req, res) {
-//     var gender = res.query.gender;
-//     // var minage = res.query.minage;
-//     // var maxage = res.query.maxage;
-//     // var country = res.query.Country;
-//     con.query(sql, function (err, result, fields) {
-//         let sql = "SELECT * FROM member where Gender =" + gender + " ORDER BY Rand() limit 4";
-//         if (err) console.log("query is not excuted. select fail...\n" + err);
-//         else res.render("pair", { data: result });
-//     });
-// });
 
 app.get("/chatroom", function (req, res) {
     res.render("chatroom");
